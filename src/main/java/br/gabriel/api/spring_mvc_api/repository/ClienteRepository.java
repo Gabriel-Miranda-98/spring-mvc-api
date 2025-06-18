82c66d743de910517dbd12a1a55261fdcfbf646b
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2025-06-17 21:27:28 -0300

    feat(clientes): Repositorio de cliente

---
+++ src/main/java/br/gabriel/api/spring_mvc_api/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+package br.gabriel.api.spring_mvc_api.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.gabriel.api.spring_mvc_api.model.Cliente;
+
+
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+    Optional<Cliente> findByName(String name);
+    @Override
+    long count();
+
+}
