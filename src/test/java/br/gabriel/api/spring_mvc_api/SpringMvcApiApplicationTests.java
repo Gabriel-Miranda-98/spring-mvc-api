20df9ad16d03691b5968ad27efe4df1dd86364b1
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2025-06-17 21:01:54 -0300

    initial commit

---
+++ src/test/java/br/gabriel/api/spring_mvc_api/SpringMvcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gabriel.api.spring_mvc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
